baade6db2746cf22477bdaa147a6c5be47f869f9
vanthanhdao <daothanh1411@gmail.com>
2023-10-16 09:54:38 +0700

    second commit

---
+++ server/server.js
@@ -0,0 +1,39 @@
+// File main run server
+
+//Import Libaries
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import importantQuestion from './routes/importantQuestion.js';
+import mongoose from 'mongoose';
+
+
+const app = express();
+const PORT = process.env.port || 5000;
+const MONGO_URI = "mongodb+srv://daothanh1411:Vanthanh1411@cluster0.c8gfhuj.mongodb.net/DriverLicense?retryWrites=true&w=majority"
+
+
+
+//Middleware
+app.use(bodyParser.json({ limit: '30mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }));
+app.use(cors());
+
+
+//Connect MongoDB
+mongoose
+    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB...')
+        //Listen PORT
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        });
+    })
+    .catch((err) => console.log(err));
+
+
+//Routes
+app.use('/importantQuestion', importantQuestion);
+
+

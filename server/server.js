97fba568232df13838c798337c29a74e6cb2dc19
vanthanhdao <121240957+vanthanhdao@users.noreply.github.com>
2026-06-10 17:39:27 +0700

    fix remove file .env

--- server/server.js
+++ server/server.js
@@ -1,47 +1,39 @@
 // File main run server
 
 //Import Libaries
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import Question from './routes/Question.js';
-import Sign from './routes/Sign.js';
-import Video from './routes/Video.js';
-import mongoose from 'mongoose';
-
-
-
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import Question from "./routes/Question.js";
+import Sign from "./routes/Sign.js";
+import Video from "./routes/Video.js";
+import mongoose from "mongoose";
 
 const app = express();
-const PORT = process.env.port || 5000;
-const MONGO_URI = "mongodb+srv://daothanh1411:Vanthanh1411@cluster0.c8gfhuj.mongodb.net/DriverLicense?retryWrites=true&w=majority"
-
-
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
 //Middleware
-app.use(bodyParser.json({ limit: '30mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }));
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cors());
-
 
 //Connect MongoDB
 mongoose
-    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected to MongoDB...')
-        //Listen PORT
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`)
-        });
-    })
-    .catch((err) => console.log(err));
-
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB...");
+    //Listen PORT
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
 
 //Routes
-app.use('/Question', Question);
-app.use('/Sign', Sign);
-app.use('/Video', Video);
-
+app.use("/Question", Question);
+app.use("/Sign", Sign);
+app.use("/Video", Video);
 
 // import Task from "./models/TaskModel.js";
 // const router = express.Router();
@@ -63,6 +55,3 @@ app.use('/Video', Video);
 // });
 
 // app.use('/User', User);
-
-
-
